3f28d3f7d1a067b2fda088e00c685e14c25aca2e
s3rra97@gmail.com <macarronesconchorizo>
2019-10-23 13:18:07 +0200

    LOGIN!! Falta el fondo y vincularlo a la bbdd

---
+++ Login.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>LOGIN</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <style>
+            #casilla{
+                height:230px;
+                width:500px;
+                border: solid;
+                border-width: 1px;
+                border-radius: 5px;
+                align-content: center;
+                position: absolute;
+                left: 400px;
+                top:300px;
+            }       
+            #boton3{
+                position:absolute;
+                top:485px;
+            } 
+            body{
+                background-image:url(images/espacio.gif);
+            }
+        </style>
+    </head>
+    <body> 
+        <div class="alert alert-warning" role="alert">
+            <b>BIENVENIDO AL QUIZZ!</b> Por favor introduce tus datos para <u>iniciar sesión</u> o <u>regístrate</u>
+        </div>
+        <div align="center" border="solid" border-width="1px" border-radius="7px">
+            <img src="images/planeta.jpg" id="planeta">       
+            <form method="post" action="" id="casilla">
+                <legend>DATOS PERSONALES:</legend>
+                <br>
+                Usuario: <input type="text" name="nombre" width="43" name="user" required outofocus>
+                <br>
+                <br>
+                Contraseña: <input type="password" name="password" width="40" name="pass" required autofocus>
+                <br>
+                <br>
+                <input type="reset" id="boton2" class="btn btn-outline-warning">
+                <input type="submit" id="boton1" class="btn btn-outline-success">
+            </form>
+            <input type="button" id="boton3" value="Regístrate" class="btn btn-info">
+        </div>
+        
+        <script src="js/jquery-ui.min.js" type="text/javascript"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+        
+        
+        
+        <?php
+        /*
+            include('./funciones.php');
+            $mysqli = conectaBBDD();
+            
+            $nombre = $_POST['user'];
+            $pass = $_POST['pass'];
+            
+            $consulta_acceso = $mysqli_query($mysqli, "SELECT User FROM usuarios WHERE User = '$nombre' AND Password = '$pass'");
+            
+            if (!$consulta_acceso){
+                echo "Usuario o contraseñas incorrectos: ".mysqli_error($mysqli);
+            }else{
+                print "Bienvenido";
+            }
+         */ 
+        ?>
+    </body>
+</html>
